6b5eaea58f46a95e1126e35e97b761b7217045d6
Akira Otaka <hydrangeas.project@gmail.com>
2025-03-19 23:25:43 +0900

    Fix Terraform validation errors: update function_parameters format and dotnet_version format

--- src/MachineLog.Infrastructure/modules/app-service/main.tf
+++ src/MachineLog.Infrastructure/modules/app-service/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_windows_web_app" "this" {
     health_check_path   = "/health"
 
     application_stack {
-      dotnet_version = "8.0"
+      dotnet_version = "v8.0"
     }
   }
 
